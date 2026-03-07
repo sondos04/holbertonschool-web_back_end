819e89bf69543f745391c28ea1782187346a292e
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-03-07 04:08:31 +0300

    Refactor log_stats.py for improved readability

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,26 +1,25 @@
 #!/usr/bin/env python3
-"""
-Provides some stats about Nginx logs stored in MongoDB
-"""
-
+"""Log stats"""
 from pymongo import MongoClient
 
+
 if __name__ == "__main__":
-    # الاتصال بقاعدة البيانات
-    client = MongoClient('mongodb://127.0.0.1:27017')
+    client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    # عدد كل الـ documents
     logs_count = collection.count_documents({})
-    print(f"{logs_count} logs")
-
-    # عدد كل method
+    print("{} logs".format(logs_count))
     print("Methods:")
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
     for method in methods:
         count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+        print("	method {}: {}".format(method, count))
 
-    # عدد GET مع path = /status
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
-    print(f"{status_check} status check")
+    status_count = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+    })
+
+    print("{} status check".format(status_count))
