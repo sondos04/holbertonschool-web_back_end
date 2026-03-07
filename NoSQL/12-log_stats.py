9c4b70ee0b3c2ba13b055c257638ebdea2254b92
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2026-03-07 04:07:04 +0300

    Refactor log_stats.py to use collection variable

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,21 +1,26 @@
 #!/usr/bin/env python3
-""" Provides some stats about Nginx logs stored in MongoDB """
+"""
+Provides some stats about Nginx logs stored in MongoDB
+"""
+
 from pymongo import MongoClient
 
 if __name__ == "__main__":
+    # الاتصال بقاعدة البيانات
     client = MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs.nginx
+    collection = client.logs.nginx
 
-    total_logs = db.count_documents({})
-    print(f"{total_logs} logs")
+    # عدد كل الـ documents
+    logs_count = collection.count_documents({})
+    print(f"{logs_count} logs")
 
+    # عدد كل method
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     for method in methods:
-        count = db.count_documents({"method": method})
+        count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
 
-    status_check_count = db.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check_count} status check")
+    # عدد GET مع path = /status
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
